b10e7670fbc8eb014a719445c168187b7b196837
biloa2005 <biloaphilemon@gmail.com>
2026-06-27 23:10:45 +0200

    premiere connection au backend

--- Desktop/AAAA/frontend Emilie/app/page.tsx
+++ Desktop/AAAA/frontend Emilie/app/page.tsx
@@ -1,10 +1,27 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect, useState } from "react";
+//MONTANT MA  DETTE
 
 export default function Home() {
+  const [product, setProduct] = useState<number>(0);
+useEffect(() => {
+  async function totalDebt() {
+    try {
+     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/mydebt/total`);
+        const data = await response.text();
+        setProduct(Number(data));
+        
+    } catch (err) {
+      console.error("erreur sur le montant de ma dette", err);
+    }
+  }
+  totalDebt();
+}, []);
   return (
   
     <div className="p-10">
       <button className="btn btn-primary">Mon bouton DaisyUI</button>
+      <div>{product}</div>
     </div>
   
 
--- Desktop/AAAA/frontend Emilie/components/dashboard/Info.tsx
+++ Desktop/AAAA/frontend Emilie/components/dashboard/Info.tsx
@@ -1,28 +1,48 @@
-import React from "react";
-//nom des different meta info
-const metaData = [
-  { title: "dettes des clients",nombre:2 },
-  { title: "mes dettes",nombre:2 },
-  { title: "produit deteriorer",nombre:2 },
-  { title: "nombre de produits",nombre:2 },
-];
+"use client";
+import React, { useEffect, useState } from "react";
+
 function Info() {
+  
+  const [product, setProduct] = useState<number>(0);
+
+  
+  useEffect(() => {
+    async function totalDebt() {
+      try {
+        //  l'URL API
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/mydebt/total`);
+        
+        // mon api nestjs renvoie un chiffre
+        const textData = await response.text();
+        setProduct(Number(textData));
+console.log(product)
+
+        //cry et catch gere les erreur
+        
+      } catch (err) {
+        console.error("Erreur sur le montant de ma dette", err);
+      }
+    }
+    totalDebt();
+  }, []);
+
   return (
     <div>
-      <ul className="grid grid-cols-1 items-center flex-1 flex justify-center lg:grid-cols-4 gap-10 mt-5 w-full">
-        {metaData.map((data) => (
-          <li key={data.title} className="rounded-xl bg-white p-6 shadow-xl flex items-center justify-center">
-            
-           <h2 className="text-base grid grid-cols-1"> 
-           <div className="flex justify-center items-center"> {data.title}</div> 
-            <div className="flex justify-center items-center">{data.nombre}</div>
-            </h2>
-            
-           </li>
-        ))}
-      </ul>
+      <div className="grid grid-cols-1 items-center flex-1 flex justify-center lg:grid-cols-4 gap-10 mt-5 w-full">
+        <div className="rounded-xl bg-white p-6 shadow-xl flex items-center justify-center">
+          <h2 className="text-base grid grid-cols-1">
+            <div className="flex justify-center items-center">
+              Montant de mes dettes
+            </div>
+            <div className="flex justify-center items-center text-accent text-2xl font-bold">
+              {product}
+            </div>
+          </h2>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Info;
+
